be79cf9291eb5f33204fc15cc42895dc433255b0
Tyler Malone <tyler@pylon.com>
2019-03-20 13:57:29 -0400

    [iOS] Adds getVolume()

--- ios/ReactNativeVolumeController/ReactNativeVolumeController.h
+++ ios/ReactNativeVolumeController/ReactNativeVolumeController.h
@@ -1,16 +1,13 @@
-//
-//  ReactNativeVolumeController.h
 //  ReactNativeVolumeController
 //
 //  Created by Tyler Malone on 03/18/19
-//  Copyright © 2019 . All rights reserved.
+//  Copyright © 2019. All rights reserved.
 //
 
 #import <React/RCTBridgeModule.h>
 
 @interface ReactNativeVolumeController : NSObject <RCTBridgeModule>
-@property(nonatomic) BOOL showsVolumeSlider;
-@property(nonatomic) BOOL showsRouteButton;
 
+- (void)initVolumeView;
 - (void)setVolume:(float)volumeValue;
 @end
--- ios/ReactNativeVolumeController/ReactNativeVolumeController.m
+++ ios/ReactNativeVolumeController/ReactNativeVolumeController.m
@@ -2,8 +2,8 @@
 //  ReactNativeVolumeController.m
 //  ReactNativeVolumeController
 //
-//  Created by Victor C Tavernari on 01/04/17.
-//  Copyright © 2017 Tavernari. All rights reserved.
+//  Created by Tyler Malone on 03/18/19.
+//  Copyright © 2019. All rights reserved.
 //
 
 #import "ReactNativeVolumeController.h"
@@ -11,13 +11,22 @@
 #import <AVFoundation/AVFoundation.h>
 #import <MediaPlayer/MediaPlayer.h>
 
-@implementation ReactNativeVolumeController
+@implementation ReactNativeVolumeController {
+    MPVolumeView *volumeView;
+    UISlider *volumeViewSlider;
+}
 
-RCT_EXPORT_MODULE()
+- (instancetype)init{
+    self = [super init];
+    [self initVolumeView];
+    return self;
+}
 
-- (void)setVolume:(float)volumeValue {
-    MPVolumeView *volumeView = [[MPVolumeView alloc] init];
-    UISlider *volumeViewSlider = nil;
+
+- (void)initVolumeView{
+    volumeView = [[MPVolumeView alloc] init];
+    volumeView.showsRouteButton = NO;
+    volumeView.showsVolumeSlider = NO;
     
     for (UIView *view in volumeView.subviews) {
         if ([view isKindOfClass:[UISlider class]]) {
@@ -25,18 +34,26 @@ RCT_EXPORT_MODULE()
             break;
         }
     }
-    
-    _showsRouteButton = false;
-    _showsVolumeSlider = false;
-    
+}
+
+- (void)setVolume:(float)volumeValue {
+//    volumeViewSlider = nil;
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.01 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         volumeViewSlider.value = volumeValue;
     });
 }
 
-RCT_EXPORT_METHOD(updateIOS:(float)value)
+RCT_EXPORT_MODULE()
+
+RCT_EXPORT_METHOD(change:(float)value)
 {
     [self setVolume:value];
+}
+
+RCT_EXPORT_METHOD(getVolume:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject){
+    dispatch_sync(dispatch_get_main_queue(), ^{
+        resolve([NSNumber numberWithFloat:[volumeViewSlider value]]);
+    });
 }
 
 @end
