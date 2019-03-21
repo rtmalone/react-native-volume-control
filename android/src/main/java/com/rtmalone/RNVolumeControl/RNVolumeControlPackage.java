a27ba3c6c421036ccd3d649b0a7478dde4b7174c
Tyler Malone <tyler@pylon.com>
2019-03-21 16:54:44 -0400

    [Android] updates package name

--- android/src/main/java/com/rtmalone/RNVolumeControl/RNVolumeControlPackage.java
+++ android/src/main/java/com/rtmalone/RNVolumeControl/RNVolumeControlPackage.java
@@ -1,17 +1,21 @@
-package com.tavernari.volumecontroller;
+package com.rtmalone.RNVolumeControl;
 
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
+import com.facebook.react.bridge.JavaScriptModule;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class ReactNativeVolumeControllerPackage implements ReactPackage {
+public class RNVolumeControlPackage implements ReactPackage {
 
-  private ReactNativeVolumeControllerModule volumeControllerModule;
+  private RNVolumeControlModule volumeControllerModule;
+
+  public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList(); }
 
   @Override
   public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
@@ -22,7 +26,7 @@ public class ReactNativeVolumeControllerPackage implements ReactPackage {
   public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
     List<NativeModule> modules = new ArrayList<>();
 
-    volumeControllerModule = new ReactNativeVolumeControllerModule(reactContext);
+    volumeControllerModule = new RNVolumeControlModule(reactContext);
 
     modules.add(volumeControllerModule);
 
